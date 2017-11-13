b089b6d9bb469dc53b5922790f20c4b01b9e176c
Raj Manvar <raj.manvar@students.iiit.ac.in>
2017-11-13 16:48:07 +0530

    adding assignment

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,51 @@
-#include <iostream>
+/*
+ * USER: Raj Manvar
+ * LANG: C++
+ *  */
 #include <bits/stdc++.h>
-
+#include <unistd.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define DRT()  int t; cin>>t; while(t--)
+#define TRACE
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
 using namespace std;
+typedef long long int lli;
+
 
 int main()
 {
-    int te = (2>3);
-    cout<<te<<endl;
-    if (0||2)
-        cout<<"hello";
+    class test 
+    {
+        public:
+        void foo(int x)
+        {
+            cout<<x<<endl;
+        }
+
+        void foo(double x)
+        {
+            cout<<x<<endl;
+        }
+
+    };
+
+    test* a = new test();
+    a->foo(5);
 }
